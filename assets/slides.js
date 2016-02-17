cc93de418ea904d48ebaaca82e3900816b46c05f
Marco Schmalz <marco@schess.ch>
2016-02-17 20:04:04 +0100

    Slides templates

---
+++ assets/slides.js
@@ -0,0 +1,11 @@
+var slideshow = remark.create({
+  highlightStyle: "default",
+  navigation: {
+    scroll: false,
+    touch: false,
+    click: true
+  },
+  ratio: "4:3",
+  slideNumberFormat: '%current%',
+  countIncrementalSlides: false
+});
