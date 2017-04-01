f42b25b85bc0831c5e6be31681900a53dc1c9703
Marco Schmalz <marco@schess.ch>
2017-04-01 08:47:42 +0200

    Auto-commit: Aufgabe 1 (dicts)

---
+++ tsbe-2017fs/scri/session4/aufgabe1.py
@@ -0,0 +1,19 @@
+kantone = {
+  'Bern': 'BE',
+  'Waadt': 'VD',
+  'Appenzell Ausserrhoden': 'AR',
+  'Wallis': 'VS',
+}
+
+hauptorte = {
+  'BE': 'Bern',
+  'VD': 'Lausanne',
+  'AR': 'Herisau',
+  'SG': 'St. Gallen',
+}
+
+# 1. Gib für alle Kantone den jeweiligen Hauptort aus
+# Waadt: Lausanne
+
+# 2. Mach einen neuen Dict, in welchem man den Kantonsnamen für jeden Hauptort
+# nachschauen kann.
