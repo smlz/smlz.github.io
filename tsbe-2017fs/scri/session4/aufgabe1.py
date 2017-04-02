94341ffd901b74d8a25344664784b9491137f039
Marco Schmalz <marco@schess.ch>
2017-04-02 22:49:49 +0200

    Session 4: Lösung Aufgabe 1

--- tsbe-2017fs/scri/session4/aufgabe1.py
+++ tsbe-2017fs/scri/session4/aufgabe1.py
@@ -4,7 +4,6 @@ kantone = {
   'Appenzell Ausserrhoden': 'AR',
   'Wallis': 'VS',
 }
-
 hauptorte = {
   'BE': 'Bern',
   'VD': 'Lausanne',
@@ -15,5 +14,27 @@ hauptorte = {
 # 1. Gib für alle Kantone den jeweiligen Hauptort aus
 # Waadt: Lausanne
 
+for kantonsname, kuerzel in kantone.items():
+    # print(kantonsname, kuerzel)
+    if kuerzel in hauptorte:
+        print(kantonsname, hauptorte[kuerzel])
+
 # 2. Mach einen neuen Dict, in welchem man den Kantonsnamen für jeden Hauptort
 # nachschauen kann.
+
+kanton_fuer_hauptorte = {}
+
+for kantonsname, kuerzel in kantone.items():
+    if kuerzel in hauptorte:
+        hauptort = hauptorte[kuerzel]   # Wert aus dem dict hauptorte holen
+        kanton_fuer_hauptorte[hauptort] = kantonsname # Wert in dict updaten
+
+# Als Einzeiler mit dict-comprenensions
+kanton_fuer_hauptorte = { hauptorte[kuerzel]: kanton
+                          for kanton, kuerzel in kantone.items()
+                          if kuerzel in hauptorte }
+
+print(kanton_fuer_hauptorte)
+
+for hauptort, kantonsname in kanton_fuer_hauptorte.items():
+    print(f"{hauptort} ist der Hauptort des Kantons {kantonsname}")
