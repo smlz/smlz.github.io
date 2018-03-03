6ea024875bc52224430b4a009c683f8ae1af6db2
Marco Schmalz <marco@schess.ch>
2018-03-03 11:14:17 +0100

    Auto-commit: f-Strings

---
+++ tsbe-2018fs/scri/session1/fstrings.py
@@ -0,0 +1,15 @@
+two = 2
+
+print(f"""Es gibt {two:b} Arten von Leuten. Diejenigen die Binärzahlen verstehen.
+Und die anderen.""")
+
+zahl = 255
+print(f"255 hexadezimal {zahl:x}")
+
+e = 2.718281828459045
+
+print(f"Eulersche Zahl: {e:5.2f}")
+
+vorname = "Heidi"
+
+print(f"Liebe {vorname}")
