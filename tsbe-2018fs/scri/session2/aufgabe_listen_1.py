b607208f4233722029af441ad789376aa99b1c87
Marco Schmalz <marco@schess.ch>
2018-03-10 11:23:32 +0100

    Auto-commit: Lösung Aufgabe 1

--- tsbe-2018fs/scri/session2/aufgabe_listen_1.py
+++ tsbe-2018fs/scri/session2/aufgabe_listen_1.py
@@ -1,6 +1,10 @@
+ziffern = ["null", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun"]
 
 def ziffer_als_wort(ziffer):
-    pass
+    if 0 <= ziffer < 10:
+        return ziffern[ziffer]
+    else:
+        return "error"
 
 assert(ziffer_als_wort(1) == 'eins')
 assert(ziffer_als_wort(7) == 'sieben')
