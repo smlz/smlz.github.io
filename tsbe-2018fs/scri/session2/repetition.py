124e86b8f0aedb36af3f89f5399ec1b6e6862afc
Marco Schmalz <marco@schess.ch>
2018-03-10 08:19:49 +0100

    Auto-commit: Repetition

---
+++ tsbe-2018fs/scri/session2/repetition.py
@@ -0,0 +1,12 @@
+name = "Marco"
+
+anzahl_dienstjahre = 3
+
+haelfte_erreicht = False
+
+print(f"{name} hat bereits {anzahl_dienstjahre} Dienstjahre geleistet")
+
+if haelfte_erreicht:
+    print("Schon über den Berg")
+else:
+    print("...")
