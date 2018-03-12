503e8fd5ad610fe6b4ee7a101b689a4dbef94d6b
Marco Schmalz <marco@schess.ch>
2018-03-12 14:04:54 +0100

    Auto-commit: Mehr Text

---
+++ tfbern-2018fs/304/session1/mehr_text.py
@@ -0,0 +1,22 @@
+mitteilung = """Lieber Kurt
+
+Vielen Dank für deinen Brief. Es hat mich sehr gefreut, 
+dass es dir gut geht.
+
+Lieber Gruss
+Marlen
+"""
+print(mitteilung)
+
+sql = '''SELECT name, age, mark, avg(tutu)
+FROM users
+JOIN ...
+ORDER BY name
+GROUP BY id
+'''
+
+print("\t1\t2\t3\t4\n\t5")
+print('Sarcasm is "just great"')
+print('Paul\'s Bakery')
+
+print("Länge der Mitteilung", len(mitteilung))
