a48bdb57c653a3cd8e26c234597ac562bd773f8c
Marco Schmalz <marco@schess.ch>
2018-03-12 13:46:11 +0100

    Auto-commit: Variabeln definieren

---
+++ tfbern-2018fs/304/session1/variabeln.py
@@ -0,0 +1,15 @@
+x = 2.0
+y = x**2 + 1
+
+print("y(2.0) =",  y)
+
+name = "Marco"
+anzahl_finger = 10
+
+print(name, "hat", anzahl_finger, "Finger")
+
+
+personen_im_raum = 14
+finger_im_raum = personen_im_raum * 10
+
+print("Es hat", finger_im_raum, "Finger im Raum")
