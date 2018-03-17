dee58341ab823d41fc3f038b70cce44ddd258dbf
Marco Schmalz <marco@schess.ch>
2018-03-17 12:03:54 +0100

    Auto-commit: Listen sortieren

---
+++ tsbe-2018fs/scri/session3/listen.py
@@ -0,0 +1,11 @@
+
+zahlen = [3, 7, 2, 7, 8, 3]
+
+# Bevorzugte Variante:
+sortierte_zahlen = sorted(zahlen)  # kreiert eine sortierte Kopie von zahlen
+
+print(sortierte_zahlen)
+
+zahlen.sort()   # sortiert die Liste zahlen selber
+
+print(zahlen)
