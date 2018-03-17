575db8332c2a16a210ca6a199836025c1d48c64b
Marco Schmalz <marco@schess.ch>
2018-03-17 12:44:21 +0100

    Auto-commit: Aufgabe: Kantonshauptorte (dicts)

---
+++ tsbe-2018fs/scri/session3/aufgabe_kantone.py
@@ -0,0 +1,11 @@
+
+kuerzel = input("Geben sie ein Kantonskürzel ein: ")
+
+
+# Beispielsession:
+# Geben sie ein Kantonskürzel ein: GR
+# Der Hauptort ders Kantons Graubünden ist Chur.
+# Geben sie ein Kantonskürzel ein: AB
+# Fehler: Kein Kanton mit dem Kürzel 'AB' gefunden
+# Geben sie ein Kantonskürzel ein: q
+# Auf wiedersehen...
