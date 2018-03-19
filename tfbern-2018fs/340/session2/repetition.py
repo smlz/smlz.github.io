9176b939d3292f77d27c7af1ae4cb9b1a3aa7460
Marco Schmalz <marco@schess.ch>
2018-03-19 13:18:33 +0100

    Auto-commit: Repetition

---
+++ tfbern-2018fs/340/session2/repetition.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+mitteilung = "Guten Tag"
+
+zwei = 1 + 1
+
+if zwei == 2:
+    print(f"zwei: {zwei}, mitteilung: {mitteilung}")
+elif zwei == 'zwei':
+    print("Hä!")
+else:
+    print("Wir sind im else")
+    print("Wir sind immer noch im else")
+
+print("Fertig")
