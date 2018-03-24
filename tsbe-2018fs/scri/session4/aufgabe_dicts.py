0534a43b45bb0387da2c31371b335d7bf6a4d2cb
Marco Schmalz <marco@schess.ch>
2018-03-24 11:04:13 +0100

    Auto-commit: Lösung Aufgabe dicts

--- tsbe-2018fs/scri/session4/aufgabe_dicts.py
+++ tsbe-2018fs/scri/session4/aufgabe_dicts.py
@@ -1,5 +1,32 @@
 # while loop der solange läuft, bis der user 'quit' eingibt
 
-kuerzel = input("Zu welchem Kanton möchtest du Informationen? ")
+kantone = {'BL': 'Basel Landschaft', 'GR': 'Graubünden', 'BE': 'Bern'}
+hauptorte = {'BL': 'Liestal', 'GR': 'Chur', 'BE': 'Bern', 'VD': 'Lausanne'}
+einwohner = {'BL': 220000, 'GR': 430000, 'BE': 1000000, 'AI': 16000}
+amtssprachen = {'BL': ['de'], 'GR': ['de', 'rm', 'it'], 'BE': ['de', 'fr']}
+sprachen = {'de': 'Deutsch', 'fr': 'Französisch', 'it': 'Italienisch', 'rm': 'Romanisch'}
 
-# print(... kantonsname, hauptorte, einwohnerzahl, amtssprachen (Liste)
+running = True
+while running:
+    kuerzel = input("Zu welchem Kanton möchtest du Informationen? ")
+    kuerzel = kuerzel.strip().upper()
+    if kuerzel in "QUIT Q EXIT X".split():
+        running = False
+    else:
+        kanton = kantone.get(kuerzel, kuerzel)
+        print(f"Informationen zum Kanton {kanton}:")
+        if kuerzel in hauptorte:
+            print(f"   * Hauptort: {hauptorte[kuerzel]}")
+        if kuerzel in einwohner:
+            print(f"   * Einwohnerzahl: {einwohner[kuerzel]}")
+        if kuerzel in amtssprachen:
+            #print(f"   * Amtssprachen:")
+            schoenere_sprachen = []
+            for code in amtssprachen[kuerzel]:
+                #print(f"     - {sprachen[code]}")
+                schoenere_sprachen.append(sprachen[code])
+
+            print(f"   * Amtssprachen:", ', '.join(schoenere_sprachen))
+
+
+
