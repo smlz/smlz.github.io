44412077e133167745a0e049628a616652fc24b2
Marco Schmalz <marco@schess.ch>
2018-03-24 09:12:22 +0100

    Auto-commit: Aufgabe dicts

---
+++ tsbe-2018fs/scri/session4/aufgabe_dicts.py
@@ -0,0 +1,5 @@
+# while loop der solange läuft, bis der user 'quit' eingibt
+
+kuerzel = input("Zu welchem Kanton möchtest du Informationen? ")
+
+# print(... kantonsname, hauptorte, einwohnerzahl, amtssprachen (Liste)
