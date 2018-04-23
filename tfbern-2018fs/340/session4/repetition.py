ea1355e2eee6cca9dbfd29612c1da106797e8388
Marco Schmalz <marco@schess.ch>
2018-04-23 13:10:16 +0200

    Auto-commit: Repetition

---
+++ tfbern-2018fs/340/session4/repetition.py
@@ -0,0 +1,13 @@
+
+buchstaben = ['a', 'b', 'c', 'd']
+
+buchstaben.append('e')
+
+for i, c in enumerate(buchstaben, 1):
+    print(f"{i}. Buchstabe: {c}")
+
+
+plz = {
+    3014: 'Bern',
+    9063: 'Stein AR',
+}
