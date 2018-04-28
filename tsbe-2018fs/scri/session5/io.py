030da976c87deb685e776c0670ea052641ca0441
Marco Schmalz <marco@schess.ch>
2018-04-28 11:30:05 +0200

    Auto-commit: Kommandozeilenargumente und Dateiüberprüfung

--- tsbe-2018fs/scri/session5/io.py
+++ tsbe-2018fs/scri/session5/io.py
@@ -1,13 +1,25 @@
+import os.path
+import sys
+
+if len(sys.argv) != 2:
+    print(f"Error! Usage python3 {sys.argv[0]} <FILENAME>")
+    sys.exit(1)
+filename = sys.argv[1]
+
+if not os.path.isfile(filename):
+    print(f"Error! File '{filename}' not found")
+    sys.exit(2)
+
 entries = []
 
-f = open('test.csv')
+f = open(filename)
 for line in f:
     line = line.strip()
     if line:
         entries.append(line.split(','))
 f.close()
 
-out = open('test.csv', 'w')    # Achtung, löscht den Inhalt der Datei!
+out = open(filename, 'w')    # Achtung, löscht den Inhalt der Datei!
 
 for entry in sorted(entries):
     print(','.join(entry), file=out)
